0637bb2b1a939d0f813a5ae63c00084f312ef7e2
0x6673 <frederichlstine@gmail.com>
2022-08-22 21:37:05 -0400

    Adding initial BSPWM setup

--- link.sh
+++ link.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
+# Link VIM files
 ln -s ~/.files/vim/.vimrc ~/.vimrc
 rm -rf ~/.vim
 ln -s ~/.files/vim/.vim ~/.vim
-
-
---
+++ linkbspwm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Link BSPWM related files
+ln -s ~/.files/bspwm/ ~/.config/bspwm
+ln -s ~/.files/sxhkd/ ~/.config/sxhkd
+ln -s ~/.files/polybar ~/.config/polybar
